0c3cfc442d74e88f5d300dc63d02d4f7ab275395
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-07-04 21:40:24 +0100

    BB 4.2 adding price field to database and styling product listing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160704134019) do
+ActiveRecord::Schema.define(version: 20160704203740) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20160704134019) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.string   "category"
+    t.decimal  "price"
   end
 
 end
