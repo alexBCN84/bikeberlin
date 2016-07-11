27d39a34d83ce06a0cbc65375c53d4ad7d7f2e9e
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-07-11 17:19:24 +0100

    BB 5.3 preparing Heroku to send emails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,19 @@
 class StaticPagesController < ApplicationController
   def index
-end
+	end
 
   def landing_page
   	@products = Product.limit(3)
   end
+
+  def thank_you   
+  @name = params[:name]   
+  @email = params[:email]   
+  @message = params[:message]   
+  ActionMailer::Base.mail(:from => @email,       
+    :to => 'alejandro.ginesmartinez@gmail.com',       
+    :subject => "A new contact form message from #{@name}",       
+    :body => @message).deliver_now 
+	end
+
 end
