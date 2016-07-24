34c6d2010d97a67bef08beccbe4bd65de49585fa
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-07-24 20:27:47 +0100

    abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    
+    if user.admin?
+      can :manage, :all
+    else
+
+      can :manage, User, id: user.id
+ 
+    end
   end
 end
