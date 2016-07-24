7845cad289ecf4aad1871d64a617189176979e0e
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-07-24 12:03:29 +0100

    fixing authorisation uses on production environment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,12 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-   
+    if user.admin?
+    	can :manage, :all
+    else
+    	can :read, :products
+    	can[:read, :upadte], User, id: user.id
+    	can :update, :comments
+    end
   end 
 end
