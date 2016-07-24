4223be35629914e4279c2748a72bf1986495fa88
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-07-24 20:23:56 +0100

    delete account

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,6 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    if user.admin?
-      can :manage, :all
-    else
-
-      can :manage, User, id: user.id
-      cannot [:new, :create, :edit, :update, :destroy], Product
-      cannot :destroy, Comment
-
-    end
+    
   end
 end
