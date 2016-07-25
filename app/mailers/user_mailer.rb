1d73ca91c8d9bf775f490a4a14f7c7ba14ee3d0c
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-07-25 11:53:26 +0100

    BB 5.11 Creating automated welcoming email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
         :to => 'alejandro.ginesmartinez@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  	@appname = "Bike Berlin"
+  	mail( :to => user.email,
+  				:subject => "Welcome to #{@appname}!")
+  end
 end
