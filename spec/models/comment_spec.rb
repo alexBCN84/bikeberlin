2d9cfbb0c2504d580dbeea4b81a1ea582ee7127f
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-08-18 16:35:21 +0200

    BB 6.2 modifications for unit tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,23 +4,25 @@ describe Comment do
 		before do
 			@product = Product.create!(name: "Best bike", price: "3435") 
   		@user = User.create!(email: "alejandro.ginesmartinez@gmail.com", password: "AGM39432079m")
-			@comment = Comment.create!(user: @user, product: @product, body: "This is an absolutely excellent product", rating: 3)
 		end
 
 		it "comment not valid because of body missing" do
-			expect(Comment.new(body: "")).not_to be_valid
+			@comment = Comment.new(user: @user, product: @product, rating: 3)
+			expect(@comment).not_to be_valid
 		end
 
-		it "comment not valid because of user_id missing" do
-			expect(Comment.new(user_id:"")).to_not be_valid	
+		it "comment not valid because of user missing" do
+			@comment = Comment.new(body: "This is an absolutely excellent product", product: @product, rating: 3)
+			expect(@comment).not_to be_valid
 		end
 
 		it "comment not valid because of rating missing" do
-			expect(Comment.new(rating:"")).to_not be_valid	
+			@comment = Comment.new(user: @user, body: "This is an absolutely excellent product", product: @product)
+			expect(@comment).not_to be_valid
 		end
 
 		it "comment valid" do
-			expect(@comment).to be_valid
+			expect(Comment.new(user: @user, product: @product, body: "This is an absolutely excellent product", rating: 3)).to be_valid
 		end
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,23 @@
 require 'rails_helper'
 describe User do
 	context "when creating user" do
-		before do
-			@user = User.create!(email: "alejandro.ginesmartinez@gmail.com", password: "AGM39432079m")
-		end
-
 		it "user cannot be created without an email" do
-			expect(User.new).not_to be_valid
+			@user = User.new(password: "alexandros")
+			expect(@user).not_to be_valid
 		end
 
-		it "created with a not valid formatted email" do
-			expect(User.new).to_not be_valid	
+		it "created with a not valid formated email" do
+			@user = User.new(email: "thisisnotavalidemail", password: "alexandros")
+			expect(@user).to_not be_valid	
 		end
 
 		it "password not supplied" do
-			expect(User.new).to_not be_valid
+			@user = User.new(email: "alejandro.ginesmartinez@gmail.com")
+			expect(@user).to_not be_valid
 		end
 
 		it "all information is valid" do
-			expect(@user).to be_valid
+			expect(User.new(email: "alejandro.ginesmartinez@gmail.com", password: "alexandros")).to be_valid
 		end
 	end
 end
