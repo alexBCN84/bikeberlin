b0a059bb9d4db9b8f6d8e8728a5d7b46a2200ee8
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-09-06 18:32:44 +0200

    BB 6.6 fixing routes payment controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,44 +1,30 @@
 class PaymentsController < ApplicationController
-	# POST /payments/payment_created
-	def create
-		#declaring instance variable to find product by reading hidden field's params in the stripe checkout button form.
-		@product = Product.find(params[:product_id])
-		#declaring instance variable to refer to the current user by using Devise helper.
-		@user = current_user
 
-		# Geting credit card details submitted by the form
-		token = params[:stripeToken]
-  	# Create the charge on Stripe's servers - this will charge the user's card
-  	begin
-    	charge = Stripe::Charge.create(
-    		#We access product's price via instance variable @product
-      	:amount => @product.price.to_i*100,
-      	:currency => "eur",
-      	:source => token,
-      	:description => params[:stripeEmail]
-    	)
-  	# Checking whether the payment has been successfull
-    # .paid is a method provided by Stripe
-    # If payment was successfull, the order is being created
-    if charge.paid
-      Order.create(
-        :product_id => @product,
-        :user_id => @user,
-        :total => @product.price
+# POST /payments
+  def create
+    token = params[:stripeToken]
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        :amount => (@product.price*100).to_i, # amount in cents, again
+        :currency => "usd",
+        :source => token,
+        :description => params[:stripeEmail]
       )
 
-    	flash[:success] = "Payment successful"
- 			redirect_to "payments/payment_successful.html.erb"
-
-
-    end
-
-    rescue Stripe::CardError => e
+      if charge.paid
+        Order.create!(:product_id => @product.id, :user_id => @user.id, :total => @product.price)
+      end
+      flash[:success] = "Payment processed successfully"
+      
+      rescue Stripe::CardError => e
+      # The card has been declined
       # The card has been declined
       body = e.json_body
       err = body[:error]
       flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-      redirect_to product_path(product)
-    end
+      end
   end
 end
