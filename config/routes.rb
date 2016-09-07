1cfe24e092ca57850af8b8170372eec2ab8734f9
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-09-07 10:57:08 +0200

    BB 6.6 fixing problems in payment controller and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   get 'static_pages/index'
   get 'static_pages/landing_page'
   post 'static_pages/thank_you'
-  post 'payments/create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -70,4 +69,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   resources :orders, only: [:index, :show, :create, :destroy]
+  post 'payments/create'
 end
