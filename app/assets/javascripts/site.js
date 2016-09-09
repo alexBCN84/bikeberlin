5a49984af70b1a264f2f0f00aa6ce23428007859
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-09-09 12:03:43 +0200

    BB 6.9 Implementing and customising Elevate Zoom jquery plugin

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -6,4 +6,12 @@ $(document).on('turbolinks:load', function (){
       return $(this).attr('data-score');
     }
   });
+  $('.img-zoom').elevateZoom({
+    zoomType: "lens",
+    lensShape: "round",
+    lensSize: 300,
+      scrollZoom: true,
+			lensFadeIn: 500,
+			lensFadeOut: 500
+  });
 });
