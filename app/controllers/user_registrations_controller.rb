5e202153e24255a6aaa49b49499c61eff5d7dc1c
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-09-10 18:07:49 +0200

    fixing user registrations controller

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -1,6 +1,7 @@
 class UserRegistrationsController < Devise::RegistrationsController
   #registering user
   def create
+  	super
     if @user.persisted?
       UserMailer.welcome(@user).deliver_now
     end
