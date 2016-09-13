d3e918f122144550598ee2239dae2c0e2978222f
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2016-09-13 19:24:03 +0200

    BB 6.10 Ajax and jquery

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,11 +1,16 @@
-$(document).on('turbolinks:load', function (){
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '/assets',
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
-  });
+
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
+    });
+};
+
+$(document).on('turbolinks:load', function() {
+  refreshRating();
   $('.img-zoom').elevateZoom({
     zoomType: "lens",
     lensShape: "round",
